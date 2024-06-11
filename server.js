7663d4f3f383775ac8674d14c58be53d91815962
brikendatah2 <114023399+brikendatah2@users.noreply.github.com>
2024-06-11 17:17:42 +0200

    Add files via upload
    add server.js

---
+++ server.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const http = require('http');
+const socketIO = require('socket.io');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
+
+const userRoutes = require('./routes/userRoutes');
+const groupRoutes = require('./Routes/groupRoutes');
+const messageRoutes = require('./Routes/MessageRoutes');
+const conversationRoutes = require('./Routes/conversationRoutes');
+const notificationRoutes = require('./Routes/notificationRoutes');
+const friendListRoutes = require('./Routes/friendListRoutes');
+
+
+const PORT = process.env.PORT || 3000;
+
+
+mongoose.connect('mongodb://localhost:27017/chatApplication')
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.error(err));
+
+
+app.use(bodyParser.json());
+
+// Routes
+app.use('/api/users', userRoutes);
+app.use('/api/groups', groupRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/conversations', conversationRoutes);
+app.use('/api/notifications', notificationRoutes);
+app.use('/api/friendlist', friendListRoutes);
+
+
+// Socket.io connection
+io.on('connection', socket => {
+  console.log('New client connected');
+  
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+
+  // Handle chat messages
+  socket.on('chat message', msg => {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+// Start server
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
